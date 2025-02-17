bcd598b8d225c4eddf08f3a73ac5146c9a835db4
dansergey <danilovsergey1@gmail.com>
2025-02-17 22:51:02 +0300

    Модуль 3

--- 3-module/5-task/index.js
+++ 3-module/5-task/index.js
@@ -1,3 +1,5 @@
 function getMinMax(str) {
-  // ваш код...
+  let a = str.split(" ").map(Number).filter(item => !isNaN(item)).sort((a, b) => a - b);
+  const result = {min: a[0], max: parseInt(a.slice(-1), 10)};
+  return result;
 }
