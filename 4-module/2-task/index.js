e489f89a48cd11d80c6c886ad0278730a993902d
dansergey <danilovsergey1@gmail.com>
2025-02-27 00:33:58 +0300

    Модуль 4

--- 4-module/2-task/index.js
+++ 4-module/2-task/index.js
@@ -1,3 +1,10 @@
-function makeDiagonalRed(table) {
-  // ваш код...
+function makeDiagonalRed(table){
+ for(let td of table.rows){ 
+  for(let th of td.cells){
+    if(th.outerText[0]==th.outerText[2]){
+      th.style.background = 'red';
+    };
+    
+  }
+ }
 }
--- 4-module/3-task/index.js
+++ 4-module/3-task/index.js
@@ -1,3 +1,34 @@
 function highlight(table) {
-  // ваш код...
+ const rows = table.querySelectorAll('tbody tr'); 
+  Array.from(rows).forEach(row => {
+  const dataAvailable = row.querySelector('td[data-available]');
+  const genderTr = row.querySelector('td:nth-child(3)');
+  const year = row.querySelector('td:nth-child(2)');
+  
+  if(dataAvailable){
+row.classList.add(dataAvailable.dataset.available === 'true' ? 'available' : 'unavailable');
+     } else {
+row.setAttribute('hidden', '');
+     }
+  
+  if(genderTr){
+    const gender = genderTr.innerHTML;
+    if(gender === 'm'){
+    row.classList.add('male');
+      } else{
+    row.classList.add('female');
+      }
+    }
+  
+  if(year){
+    const age = parseInt(year.innerHTML, 10);
+    if(age < 18)
+     {
+      row.style.textDecoration = 'line-through';
+     }
+  }
+  
+  })
+  
+
 }
